88c68b10a5c6b716aa42ad8244021f9f8fad9e27
joe <williamsthehack@gmail.com>
2023-11-24 18:07:38 +0100

    Beschreibende Kommentare hinzugefügt. Farben und Fonts aus dem DC in jeweils colors.xml und den font Ordner eingefügt

--- app/src/main/java/com/dhbw/unilife/MainActivity.java
+++ app/src/main/java/com/dhbw/unilife/MainActivity.java
@@ -3,12 +3,13 @@ package com.dhbw.unilife;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    // Die onCreate Funktion der MainActivity wird als erstes beim starten der app aufgerufen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // Layout-Datei zuweisen
     }
 }
